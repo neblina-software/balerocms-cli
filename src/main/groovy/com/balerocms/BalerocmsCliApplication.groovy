75454ac4fc711984b3dec55a1dacd624ac5693a5
Anibal Gomez <anibalgomez@balerocms.com>
2016-02-18 20:39:12 -0600

    Initial commit

---
+++ src/main/groovy/com/balerocms/BalerocmsCliApplication.groovy
@@ -0,0 +1,12 @@
+package com.balerocms
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BalerocmsCliApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run BalerocmsCliApplication, args
+	}
+}
